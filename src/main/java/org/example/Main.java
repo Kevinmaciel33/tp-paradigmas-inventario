d086294eb09dd67bffdbb462027cf82566ec8a01
Kevinmaciel33 <38921659+Kevinmaciel33@users.noreply.github.com>
2025-06-20 19:18:22 -0300

    fix spaces

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,20 +12,20 @@ import java.util.concurrent.CompletableFuture;
 public class Main {
     public static void main(String[] args) {
 
-        //Loader[] loader = { new InventoryLoader(), new RecipeLoader() };
+        Loader[] loader = { new InventoryLoader(), new RecipeLoader() };
 
 // TODO idea: hacer hilos para cargar los archivos y crons para que se actualicen cada n segundos
 // en caso de que el archivo de error, cargar un backup anterior
-        //for ( int i = 0; i < loader.length; ++i ) {
-        //    int finalI = i;
-            //CompletableFuture.runAsync(() -> {
-        //        loader[finalI].loadFile();
-            //});
-        //}
+        for ( int i = 0; i < loader.length; ++i ) {
+            int finalI = i;
+            CompletableFuture.runAsync(() -> {
+                loader[finalI].loadFile();
+            });
+        }
 
-        //final Inventory i = InventoryLoader.getData();
-        //final RecipeBook r = RecipeLoader.getData();
-        //Worker w = new Worker(i, r);
+        final Inventory i = InventoryLoader.getData();
+        final RecipeBook r = RecipeLoader.getData();
+        Worker w = new Worker(i, r);
     	Menu.ejecutarMenu();
     }
 }
