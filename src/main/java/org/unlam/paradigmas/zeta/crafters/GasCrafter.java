04a52052a2b1ed15ce58a737a12ffd44e1d117ee
Nahuel <debellisnahuel@gmail.com>
2025-06-27 01:36:37 -0300

    read type when read the element

--- src/main/java/org/unlam/paradigmas/zeta/crafters/GasCrafter.java
+++ src/main/java/org/unlam/paradigmas/zeta/crafters/GasCrafter.java
@@ -18,7 +18,7 @@ public class GasCrafter extends Crafter {
 
     @Override
     public void craft(Element element, Inventory inventory, Recipe recipe) {
-        if (shouldApply(inventory, element)) {
+        if (!shouldApply(inventory, element)) {
             throw new IllegalArgumentException("Este catalizador solo aplica a recetas de tipo " + type());
         }
 
