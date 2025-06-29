c84a98976b490272ca11ad1d6b8e29b6fa748ad8
Lautaro307 <97195752+Lautaro307@users.noreply.github.com>
2025-06-29 19:56:48 -0300

    Add files via upload

---
+++ src/test/java/org/unlam/paradigmas/zeta/querys/PrologRuleGeneratorTests.java
@@ -0,0 +1,15 @@
+package java.org.unlam.paradigmas.zeta.querys;
+
+import org.unlam.paradigmas.zeta.models.Recipe;
+import org.unlam.paradigmas.zeta.RecipeBook;
+import org.unlam.paradigmas.zeta.enums.Classification;
+import org.unlam.paradigmas.zeta.loaders.RecipeLoader;
+import org.unlam.paradigmas.zeta.models.Element;
+import org.unlam.paradigmas.zeta.models.Library;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PrologRuleGeneratorTests {
+
+}
